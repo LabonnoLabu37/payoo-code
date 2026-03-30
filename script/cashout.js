02f2b00de16063c2626eeda01cf367cf3ab690fd
unknown <you@example.com>
2026-03-30 21:04:57 +0600

    cashout functionality setup

---
+++ script/cashout.js
@@ -0,0 +1,45 @@
+
+
+document.getElementById("cashout-btn").addEventListener("click", function(){
+
+// get the agent number and validate
+const cashoutNumberInput =   document.getElementById("cashout-number");
+const cashoutNumber = cashoutNumberInput.value ;
+console.log(cashoutNumber);
+
+if(cashoutNumber.length != 11){
+    alert("Invalid Number");
+    return;
+}
+
+// get the amount
+const cashoutAmountInput = document.getElementById("cashout-amount");
+const cashoutAmount = cashoutAmountInput.value;
+console.log(cashoutAmount);
+
+// get the current balance
+const balanceElement = document.getElementById("balance");
+const balance = balanceElement.innerText;
+console.log(balance);
+
+// calculate new balance
+const newBalance =  Number(balance) - Number(cashoutAmount);
+
+if(newBalance<0){
+    alert("Invalid Amount");
+    return;
+}
+
+
+// get the pin and verify
+const cashoutPinInput = document.getElementById("cashout-pin");
+const pin = cashoutPinInput.value;
+if(pin === '1234'){
+    alert("cashout successfull")
+    console.log(newBalance);
+    balanceElement.innerText = newBalance;
+} else{
+    alert("Invalid pin");
+    return;
+}
+});
