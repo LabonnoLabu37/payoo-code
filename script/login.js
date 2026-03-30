a3200f8cf01f693dbceb8b073f093538052a4c0d
unknown <you@example.com>
2026-03-30 13:34:54 +0600

    login.js fully added

--- script/login.js
+++ script/login.js
@@ -1,12 +1,32 @@
-document.getElementById("login-btn").addEventListener("click" , function () {
+console.log("JS loaded");
+
+
+
+
+
+
+document.addEventListener("click", function () {
+
+  document.getElementById("login-btn").addEventListener("click", function () {
 
     // get the mobile number value
-const numberInput = document.getElementById("input-number");
-const contactNumber =  numberInput.value ;
-console.log(contactNumber);
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
 
-// get the pin input
-const inputPin = document.getElementById("input-pin");
-const pin = inputPin.value ;
-console.log (pin);
+    // get the pin input
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin);
+
+    // match pin and mobile number
+  if (contactNumber == "01805222906" && pin == "1234") {
+    alert("login successful");
+    window.location.assign = "home.html";
+} else {
+    alert("Login Failed");
+}
+
+  });
+
 });
